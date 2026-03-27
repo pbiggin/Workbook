d2aea315dcc0b7a0536a098e05dc8cd5be5871fe
pbiggin <pbiggin@student.unimelb.edu.au>
2026-03-27 12:36:32 +1100

    added colour randomiser

---
+++ scripts/scripts.js
@@ -0,0 +1,43 @@
+const colours = [
+  "#e040ae",
+  "#F25016",
+  "#6596F3",
+  "#83B366",
+  "#D3A4EA",
+  "#EAD094",
+  "#B2DCE2",
+  "#D7EAAC",
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+  const colourables = document.querySelectorAll(".random-colour");
+
+  colourables.forEach((colour) => {
+    const randomColour = colours[Math.floor(Math.random() * colours.length)];
+    colour.style.backgroundColor = randomColour;
+  });
+});
+
+
+   function openBook(container) {
+        const book = container.querySelector(".book");
+        const insidePages = container.querySelectorAll(".book-inside");
+        if (book) {
+          book.classList.add("open");
+        }
+        insidePages.forEach((page) => {
+          page.classList.add("open");
+        });
+      }
+
+      function closeBook(container) {
+        const book = container.querySelector(".book");
+        const insidePages = container.querySelectorAll(".book-inside");
+        if (book) {
+          book.classList.remove("open");
+        }
+        insidePages.forEach((page) => {
+          page.classList.remove("open");
+        });
+      }
+      
